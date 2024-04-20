e104728146e9f74edb8507d92d3a990b98b1f3b5
Dmitry <dmitryryabickov@yandex.ru>
2024-04-20 21:28:21 +0300

    Modifu cocotb test

--- sim/cocotb/i2c_master_tb.py
+++ sim/cocotb/i2c_master_tb.py
@@ -10,17 +10,18 @@ async def test_i2c_master(dut):
     # cocotb.start_soon(Clock(dut.i2c_clk, 20, units = 'ns').start())
 
     # Reset and enable module
-    await RisingEdge(dut.clk)
     dut.arst.value = 1
-    dut.fifo_wr_en.value = 0
     await Timer(2, units="sec")
     dut.arst.value = 0
-    dut.fifo_wr_en.value = 1
     await RisingEdge(dut.clk)
 
-    for i in range(4):
-        await Timer(50, units="sec")
+    dut.fifo_wr_en.value = 1
+
+    for i in range(8):
+        await Timer(2, units="sec")
         dut.data.value = random.randint(0, 256)
         dut.addr.value = random.randint(0, 127)
+    
+    dut.fifo_wr_en.value = 0
 
-    await Timer(10, units="sec")
+    await Timer(90, units="sec")
