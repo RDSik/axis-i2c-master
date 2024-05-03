9ae1f43f0d968fb2a86b10791b116fb53a2a898b
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2024-05-03 18:06:00 +0300

    Update Manifest.py

--- syn/vivado/Manifest.py
+++ syn/vivado/Manifest.py
@@ -1,12 +1,13 @@
 target = "xilinx"
-
 action = "synthesis"
 
 syn_device = "xc7z020clg484"
 syn_grade = "-1"
 syn_package = ""
+
 syn_top = "i2c_master"
 syn_project = "i2c_master"
+
 syn_tool = "vivado"
 
 syn_post_project_cmd = "vivado -mode tcl -source add_ip.tcl"
