769bb13d51d442cdc30008576fb73f552447964b
Dmitry Ryabikov <dmitry.ryabikov@spbpu.com>
2024-07-29 16:38:21 +0300

    Add icarus simulation with hdlmake

---
+++ sim/iverilog/Manifest.py
@@ -0,0 +1,11 @@
+action = "simulation"
+sim_tool = "iverilog"
+sim_top = "i2c_master_tb"
+
+sim_post_cmd = "vvp i2c_master_tb.vvp; gtkwave i2c_master_tb.vcd"
+
+modules = {
+    "local" : [ 
+        "../../top/tb/",
+    ],
+}
