6a652ce0c8a3876ac7808b2bdba69ccce1ae3b54
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2024-08-14 19:55:39 +0300

    Update i2c_master_tb.py

--- sim/cocotb/i2c_master_tb.py
+++ sim/cocotb/i2c_master_tb.py
@@ -5,34 +5,35 @@ from cocotb.triggers import Timer, RisingEdge, FallingEdge, ClockCycles
 
 @cocotb.test()
 async def test_i2c_master(dut):
-    dut_clk = dut.clk
-    dut_rst = dut.arst
-    dut_wr_en = dut.fifo_wr_en
-    dut_rd_en = dut.fifo_rd_en
-    dut_data = dut.data
-    dut_addr = dut.addr
+    
+    clk_per = 2
 
-    cocotb.start_soon(Clock(dut_clk, 2, units = 'sec').start())
+    cocotb.start_soon(Clock(dut.clk, clk_per, units = 'sec').start())
     # cocotb.start_soon(Clock(dut.i2c_clk, 20, units = 'sec').start())
 
-    dut_rst.value = 1
-    await Timer(2, units="sec")
-    dut_rst.value = 0
-    await Timer(2, units="sec")
+    async def rst(dut, cycles):
+        dut.arst.value = 1
+        await ClockCycles(dut.clk, cycles)
+        dut.arst.value = 0
 
-    dut_rd_en.value = 0
-
-    for i in range(4):    
-        dut_wr_en.value = 1
-        dut_data.value = random.randint(0, 255)
-        dut_addr.value = random.randint(0, 127)
-        await Timer(2, units="sec")
-        dut_wr_en.value = 0
-        await Timer(2, units="sec")
-        
+    async def data_gen():
+        for i in range(4):    
+            dut.fifo_wr_en.value = 1
+            dut.data.value = random.randint(0, 255)
+            dut.addr.value = random.randint(0, 127)
+            await RisingEdge(dut.clk)
+            dut_wr_en.value = 0
+            await RisingEdge(dut.clk)
+    
+    #------------------Order of test execution -------------------
+    await rst(dut, 1)
+    dut.fifo_wr_en.value = 0
+    await data_gen()
     for i in range(4):
         dut_rd_en.value = 1
-        await Timer(2, units="sec")
+        await Timer(clk_per, units="sec")
         dut_rd_en.value = 0
-        await Timer(2, units="sec")
+        await Timer(clk_per, units="sec")
         await Timer(40, units="sec")
+    
+    
