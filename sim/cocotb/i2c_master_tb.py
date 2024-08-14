07f656845c8291c71ef0aabbccd758029a7f806f
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2024-08-14 20:00:57 +0300

    Update i2c_master_tb.py

--- sim/cocotb/i2c_master_tb.py
+++ sim/cocotb/i2c_master_tb.py
@@ -22,7 +22,7 @@ async def test_i2c_master(dut):
             dut.data.value = random.randint(0, 255)
             dut.addr.value = random.randint(0, 127)
             await RisingEdge(dut.clk)
-            dut_wr_en.value = 0
+            dut.fifo_wr_en.value = 0
             await RisingEdge(dut.clk)
     
     #------------------Order of test execution -------------------
@@ -30,9 +30,9 @@ async def test_i2c_master(dut):
     dut.fifo_wr_en.value = 0
     await data_gen()
     for i in range(4):
-        dut_rd_en.value = 1
+        dut.fifo_rd_en.value = 1
         await Timer(clk_per, units="sec")
-        dut_rd_en.value = 0
+        dut.fifo_rd_en.value = 0
         await Timer(clk_per, units="sec")
         await Timer(40, units="sec")
     
