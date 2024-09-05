786fb3b6ea8c075d3501c3533b74c5e97c3bbdab
Dmitry <dmitryryabickov@yandex.ru>
2024-09-05 18:07:32 +0300

    Upd test

--- sim/cocotb/modelsim/i2c_master_tb.py
+++ sim/cocotb/modelsim/i2c_master_tb.py
@@ -12,35 +12,37 @@ async def reset(dut, cycles):
     await ClockCycles(dut.clk, cycles)
     dut.arst.value = 0
 
-async def write(dut):
-    dut.fifo_wr_en.value = 1
-    dut.data.value = random.randint(0, 255)
-    dut.addr.value = random.randint(0, 127)
-    await Timer(clk_per, units="ns")
-    dut.fifo_wr_en.value = 0
+async def write(dut, n):
+    for i in range(n):
+        dut.fifo_wr_en.value = 1
+        dut.data.value = random.randint(0, 255)
+        dut.addr.value = random.randint(0, 127)
+        await RisingEdge(dut.clk)
+        dut.fifo_wr_en.value = 0
+        await RisingEdge(dut.clk)
+    print(f'Write ended at {get_sim_time('ns')} ns.')
 
-async def read(dut):
-    dut.fifo_rd_en.value = 1
-    await Timer(clk_per, units="ns")
-    dut.fifo_rd_en.value = 0
+async def read(dut, n):
+    for i in range(n):
+        dut.fifo_rd_en.value = 1
+        await Timer(clk_per, units="ns")
+        dut.fifo_rd_en.value = 0
+        await Timer(clk_per, units="ns")
+        await Timer(clk_per*20, units="ns")
+    print(f'Read ended at {get_sim_time('ns')} ns.')
 
 async def init(dut, n):
-
+    
     cocotb.start_soon(Clock(dut.clk, clk_per, units = 'ns').start())
-    # cocotb.start_soon(Clock(dut.i2c_clk, 20, units = 'sec').start())
+    # cocotb.start_soon(Clock(dut.i2c_clk, 20, units = 'sec').start(
     
     dut.fifo_wr_en.value = 0
     dut.fifo_rd_en.value = 0
     await reset(dut, clk_per)
-    assert dut.fifo_empty.value == 1, f'Error fifo is not empty at {get_sim_time('ns')} ns.'
-    for i in range(n):
-        await Timer(clk_per, units="ns")
-        await write(dut)
-    assert dut.fifo_full.value == 1, f'Error fifo is not full at {get_sim_time('ns')} ns.'
-    for i in range(n):
-        await Timer(clk_per, units="ns")
-        await read(dut)
-    assert dut.fifo_empty.value == 1, f'Error fifo is not empty at {get_sim_time('ns')} ns.'
+    assert dut.fifo_empty.value == 1, f'Fifo is not empty at {get_sim_time('ns')} ns.'
+    await write(dut, n)
+    assert dut.fifo_full.value == 1, f'Fifo is not full at {get_sim_time('ns')} ns.'
+    await read(dut, n)
 
 @cocotb.test()
 async def test_i2c_master(dut):
