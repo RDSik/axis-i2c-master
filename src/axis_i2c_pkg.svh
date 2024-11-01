b7ef4ae98086bff42369354cee38f490ae385eb1
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2024-11-01 23:48:53 +0300

    Update axis_i2c_pkg.svh

--- src/axis_i2c_pkg.svh
+++ src/axis_i2c_pkg.svh
@@ -5,7 +5,7 @@ package axis_i2c_pkg;
 
     parameter I2C_DATA_WIDTH  = 8;
     parameter I2C_ADDR_WIDTH  = 7;
-    parameter AXIS_DATA_WIDTH = 16;
+    parameter AXIS_DATA_WIDTH = I2C_DATA_WIDTH * 2;
     parameter CNT_WIDTH       = $clog2(AXIS_DATA_WIDTH);
     
 endpackage
