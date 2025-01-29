eac7b697bcd53cc4c7d40cf77465eb8faca9b278
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2025-01-30 00:39:06 +0300

    Update axis_if.sv

--- src/axis_if.sv
+++ src/axis_if.sv
@@ -2,10 +2,11 @@
 
 interface axis_if;
     import axis_i2c_pkg::AXIS_DATA_WIDTH;
+#(parameter DATA_WIDTH = AXIS_DATA_WIDTH);
 
-    logic [AXIS_DATA_WIDTH-1:0] tdata;
-    logic                       tvalid;
-    logic                       tready;
+    logic [DATA_WIDTH-1:0] tdata;
+    logic                  tvalid;
+    logic                  tready;
 
     modport master (
         input  tready,
