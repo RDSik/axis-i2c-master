a39da3292267c25b4d29516943e1675a10f1e300
Dmitry <dmitryryabickov@yandex.ru>
2025-02-16 11:34:40 +0300

    Upd interface

--- tb/axis_i2c_top_if.sv
+++ tb/axis_i2c_top_if.sv
@@ -7,9 +7,9 @@ bit en_i;
 wire i2c_sda_io;
 
 logic i2c_scl_o;
-logic m_axis_tvalid;
-logic m_axis_tready;
 
 logic [7:0] m_axis_tdata;
+logic       m_axis_tvalid;
+logic       m_axis_tready;
 
 endinterface
