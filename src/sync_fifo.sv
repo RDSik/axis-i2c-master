f537468b5812e7d19f2801e759c59acbd54905d2
Dmitry <dmitryryabickov@yandex.ru>
2025-02-17 23:27:42 +0300

    upd fifo

--- src/sync_fifo.sv
+++ src/sync_fifo.sv
@@ -2,27 +2,27 @@ module sync_fifo #(
     parameter DATA_WIDTH = 16,
     parameter FIFO_DEPTH = 4
 ) (
-    input  wire                  clk,
-    input  wire                  arstn,
-    input  wire                  pop,
-    input  wire                  push,
-    input  wire [DATA_WIDTH-1:0] data_in,
-    output wire [DATA_WIDTH-1:0] data_out,
-    output wire                  empty,
-    output wire                  full
+    input  logic                  clk,
+    input  logic                  arstn,
+    input  logic                  pop,
+    input  logic                  push,
+    input  logic [DATA_WIDTH-1:0] data_in,
+    output logic [DATA_WIDTH-1:0] data_out,
+    output logic                  empty,
+    output logic                  full
 );
 
 localparam POINTER_WIDTH = $clog2(FIFO_DEPTH);
 localparam MAX_POINTER   = POINTER_WIDTH'(FIFO_DEPTH-1);
 
-reg [DATA_WIDTH-1:0] fifo [0:FIFO_DEPTH-1];
+logic [DATA_WIDTH-1:0] fifo [0:FIFO_DEPTH-1];
 
-reg [POINTER_WIDTH-1:0] rd_pointer;
-reg [POINTER_WIDTH-1:0] wr_pointer;
-reg [POINTER_WIDTH:0  ] status_cnt;
+logic [POINTER_WIDTH-1:0] rd_pointer;
+logic [POINTER_WIDTH-1:0] wr_pointer;
+logic [POINTER_WIDTH:0  ] status_cnt;
 
 // Read logic
-always @(posedge clk or negedge arstn) begin
+always_ff @(posedge clk or negedge arstn) begin
     if (~arstn) begin
         rd_pointer <= '0;
     end else if (pop) begin
@@ -31,7 +31,7 @@ always @(posedge clk or negedge arstn) begin
 end
 
 // Write logic
-always @(posedge clk or negedge arstn) begin
+always_ff @(posedge clk or negedge arstn) begin
     if (~arstn) begin
         wr_pointer <= '0;
     end else if (push) begin
@@ -40,7 +40,7 @@ always @(posedge clk or negedge arstn) begin
 end
 
 // Status counter for full and empty
-always @(posedge clk or negedge arstn) begin
+always_ff @(posedge clk or negedge arstn) begin
     if (~arstn) begin
         status_cnt <= '0;
     end else if (push && !pop && (status_cnt != FIFO_DEPTH)) begin
@@ -50,7 +50,7 @@ always @(posedge clk or negedge arstn) begin
     end
 end
 
-always @(posedge clk) begin
+always_ff @(posedge clk) begin
     if (push) begin
         fifo[wr_pointer] <= data_in;
     end
@@ -58,8 +58,8 @@ end
 
 assign data_out = fifo[rd_pointer];
 
-// assign full     = (status_cnt >= FIFO_DEPTH);
-// assign empty    = (status_cnt <= 0);
+// assign full  = (status_cnt >= FIFO_DEPTH);
+// assign empty = (status_cnt <= 0);
 
 assign full  = push ? (status_cnt >= FIFO_DEPTH - 1) : (status_cnt == FIFO_DEPTH);
 assign empty = pop ? (status_cnt <= 1) : (status_cnt == 0);
