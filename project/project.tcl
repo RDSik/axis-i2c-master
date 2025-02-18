5105cc9ec58ea5bf32b90267190e53b32f1e2dca
Dmitry <dmitryryabickov@yandex.ru>
2025-02-18 12:10:32 +0300

    Add axis_fifo to tcl

--- project/project.tcl
+++ project/project.tcl
@@ -12,6 +12,7 @@ set_property target_language $language [current_project]
 set_property top $top [current_fileset]
 
 add_files -norecurse $src_dir/axis_i2c_master.sv
+add_files -norecurse $src_dir/axis_fifo.sv
 add_files -norecurse $src_dir/axis_i2c_top.sv
 add_files -norecurse $src_dir/axis_if.sv
 add_files -norecurse $src_dir/axis_i2c_pkg.svh
