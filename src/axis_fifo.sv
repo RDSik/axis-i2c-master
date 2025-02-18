74f85475ab7266dabb277ad4a90dd1438f0be50b
Dmitry <dmitryryabickov@yandex.ru>
2025-02-18 16:51:51 +0300

    rename pointers

--- src/axis_fifo.sv
+++ src/axis_fifo.sv
@@ -14,8 +14,8 @@ localparam MAX_POINTER   = POINTER_WIDTH'(FIFO_DEPTH-1);
 
 logic [DATA_WIDTH-1:0] fifo [0:FIFO_DEPTH-1];
 
-logic [POINTER_WIDTH-1:0] rd_ptr;
-logic [POINTER_WIDTH-1:0] wr_ptr;
+logic [POINTER_WIDTH-1:0] rd_pointer;
+logic [POINTER_WIDTH-1:0] wr_pointer;
 logic [POINTER_WIDTH:0  ] status_cnt;
 
 logic pop;
@@ -26,18 +26,18 @@ logic full;
 // Read logic
 always_ff @(posedge clk_i or negedge arstn_i) begin
     if (~arstn_i) begin
-        rd_ptr <= '0;
+        rd_pointer <= '0;
     end else if (pop) begin
-        rd_ptr <= (rd_ptr == MAX_POINTER) ? '0 : rd_ptr + 1;
+        rd_pointer <= (rd_pointer == MAX_POINTER) ? '0 : rd_pointer + 1;
     end
 end
 
 // Write logic
 always_ff @(posedge clk_i or negedge arstn_i) begin
     if (~arstn_i) begin
-        wr_ptr <= '0;
+        wr_pointer <= '0;
     end else if (push) begin
-        wr_ptr <= (wr_ptr == MAX_POINTER) ? '0 : wr_ptr + 1;
+        wr_pointer <= (wr_pointer == MAX_POINTER) ? '0 : wr_pointer + 1;
     end
 end
 
@@ -54,14 +54,14 @@ end
 
 always_ff @(posedge clk_i) begin
     if (push) begin
-        fifo[wr_ptr] <= s_axis.tdata;
+        fifo[wr_pointer] <= s_axis.tdata;
     end
 end
 
 always_comb begin
     s_axis.tready = ~full;
     m_axis.tvalid = ~empty;
-    m_axis.tdata  = fifo[rd_ptr];
+    m_axis.tdata  = fifo[rd_pointer];
     push  = s_axis.tvalid & s_axis.tready;
     pop   = m_axis.tvalid & m_axis.tready;
     full  = push ? (status_cnt >= FIFO_DEPTH - 1) : (status_cnt == FIFO_DEPTH);
